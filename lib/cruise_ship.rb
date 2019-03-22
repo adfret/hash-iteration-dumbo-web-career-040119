f846be6611f6df7c3919d99f270376cedb288719
Ad Friedman <adien330@yahoo.com>
2019-03-22 18:01:06 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,11 @@ def select_winner(passengers)
   suite_e: "Crumpet the Elf"
   }
 
-passengers.each do |i,v|
-      if i == :suite_a and if v[0] == "A"
-        puts v
-      end
-end
+  passengers.each do |i,v|
+        if i == :suite_a and v[0] == "A"
+          puts v
+        end
+  end
 
 
 end
